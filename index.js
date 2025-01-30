170aaad8ecbd398df292103f5ea414ebeaf5fed1
Bunescu Fabian <bunescufabian@gmail.com>
2025-01-30 12:39:22 +0200

    hotfix on date format

--- index.js
+++ index.js
@@ -8,71 +8,69 @@ const UNSTRUCTURED = 'K';
 const STRUCTURED = 'S';
 
 app.get('/generate-qr', async (req, res) => {
-  const paymentData = {
-    format: BbitQRBillFormat.DEFAULT, // Defines the output format
-    version: BbitQRBillVersion.V2_0, // Swiss QR Bill standard version
-    amount: 100.0,
-    currency: BbitQRBillCurrency.EUR, // Currency in CHF
-    account: 'CH4431999123000889012', // IBAN account number
-    creditor: {
-        type: UNSTRUCTURED,
-        name: 'bbit gmbh',
-        address: 'Rainweg 10',
-        postalCode: '3612',
-        locality: 'Steffisburg',
-        country: 'CH',
-    },
-
-    reference: '210000000003139471430009017', // Payment reference
-    debtor: {
-        type: STRUCTURED,
-      name: 'Debtor Name',
-      street: 'Debtor Address',
-      buildingNumber: '123',
-      country: 'CH',
-      locality: 'Bern',
-      postalCode: '3000',
-    },
-    billInformation: {
-        documentNumber: "INV-202401",
-        documentDate: "2024-01-30",
-        customerReference: "CUST-123456",
-        vatNumber: "CHE-123.456.789 MWST",
-        vatDate: {
-          start: "2024-01-01",
-          end: "2024-01-31",
+    const paymentData = {
+        format: BbitQRBillFormat.DEFAULT, // Defines the output format
+        version: BbitQRBillVersion.V2_0, // Swiss QR Bill standard version
+        amount: 782.00,
+        currency: BbitQRBillCurrency.EUR, // Currency in CHF
+        account: 'CH4431999123000889012', // IBAN account number
+        creditor: {
+            type: UNSTRUCTURED,
+            name: 'bbit gmbh',
+            address: 'Rainweg 10',
+            postalCode: '3612',
+            locality: 'Steffisburg',
+            country: 'CH',
         },
-        vat: [
-          {
-            rate: 7.7,
-            netAmount: 500.00,
-          },
-          {
-            rate: 2.5,
-            netAmount: 200.00,
-          },
-        ],
-        vatImportTax: [
-          {
-            rate: 7.7,
-            vatAmount: 38.50,
-          },
-        ],
-        paymentTerms: [
-          {
-            days: 30,
-            cashDiscountPercent: 2.0,
-          },
-          {
-            days: 60,
-            cashDiscountPercent: 0.0,
-          },
-        ],
-      },
-
-    unstructuredMessage: 'Invoice 202401', // Additional payment information
-    language: BbitQRBillLanguage.EN, // Language of the QR bill
-  };
+        reference: '210000000003139471430009017', // Payment reference
+        debtor: {
+            type: STRUCTURED,
+            name: 'Debtor Name',
+            street: 'Debtor Address',
+            buildingNumber: '123',
+            country: 'CH',
+            locality: 'Bern',
+            postalCode: '3000',
+        },
+          billInformation: {
+            documentNumber: "INV-202401",
+            documentDate: "240130", // yyMMdd
+            customerReference: "CUST-123456",
+            vatNumber: "CHE-123.456.789 MWST",
+            vatDate: {
+              start: "240101", // yyMMdd
+              end: "240131", // yyMMdd
+            },
+            vat: [
+              {
+                rate: 7.7,
+                netAmount: 500.00,
+              },
+              {
+                rate: 2.5,
+                netAmount: 200.00,
+              },
+            ],
+            vatImportTax: [
+              {
+                rate: 7.7,
+                vatAmount: 38.50,
+              },
+            ],
+            paymentTerms: [
+              {
+                days: 30,
+                cashDiscountPercent: 2.0,
+              },
+              {
+                days: 60,
+                cashDiscountPercent: 0.0,
+              },
+            ],
+        },
+        unstructuredMessage: 'Invoice 202401', // Additional payment information
+        language: BbitQRBillLanguage.EN, // Language of the QR bill
+      };
 
   try {
     const qr = new BbitQRBillGenerator();
